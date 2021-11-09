edf4cbfcd1a3512e3aa81d98ac3627a20f3061e0
Johan Leirnes <johan.von.lindstrom@gmail.com>
2021-11-09 09:04:48 +0100

    Update keymap.c

--- keyboards/melody96hotiso/keymaps/bhaap/keymap.c
+++ keyboards/melody96hotiso/keymaps/bhaap/keymap.c
@@ -32,6 +32,7 @@ enum tapdance_keycodes {
 #define KC_FLXP LGUI(KC_E)
 #define KC_CAPP LGUI(LSFT(KC_4))
 #define KC_SPOT LGUI(KC_SPC)
+#define KC_MSS LGUI(LSFT(LALT(KC_L)))
 
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS),
@@ -68,7 +69,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
-        KC_ESC,  KC_BRID,   KC_BRIU, KC_MCTL, KC_SPOT, KC_F5,KC_POWER, KC_MPRV, KC_MPLY,   KC_MNXT,   KC__VOLDOWN,  KC__VOLUP,  KC__MUTE,  KC_CAPP,  KC_DEL,  KC_INS,  KC_EJCT, BR, KC_POWER,
+        KC_ESC,  KC_BRID,   KC_BRIU, KC_MCTL, KC_SPOT, KC_F5,KC_POWER, KC_MPRV, KC_MPLY,   KC_MNXT,   KC__VOLDOWN,  KC__VOLUP,  KC__MUTE,  KC_CAPP,  KC_DEL,  KC_INS,  KC_EJCT, BR, KC_MSS,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  MO(1),            KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
         KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_ENT,     KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
         KC_BSPC,       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS,             KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
